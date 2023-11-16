735fbc1a51909cf2721283054cbf4478e95cd3dd
togashi0115 <takuto.tsaxophone@gmail.com>
2023-11-16 15:15:41 +0900

    ログインページとコントローラーの進行

--- LearnEval/src/main/java/com/example/demo/controller/LaearnEvalController.java
+++ LearnEval/src/main/java/com/example/demo/controller/LaearnEvalController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
-public class LaearnEval {
+public class LaearnEvalController {
 
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
 	public String login() {
@@ -13,11 +15,13 @@ public class LaearnEval {
 		return "login";
 	}
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
-	public String checkUser(String id,String pass) {
-		if(id == "seito") {
-			return "studentlogin";
-		}else if(id == "sense") {
-			return "teacherlogin";
+	public String checkUser(String type,String id,String pass,Model model) {
+		if(type.equals("student")) {
+			return "studentmain";
+		}else if(type.equals("teacher")) {
+			return "teachermain";
+		}else{
+		model.addAttribute("login","<script>alert('一致するIDがありません');</script>");
 		}
 		return "login";
 	}
