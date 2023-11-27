f82939553b0de0bc33eba1c665aa5b245518a701
takuto0115 <takuto.tsaxophone@gmail.com>
2023-11-27 15:01:35 +0900

    11/27

--- LearnEval/src/main/java/com/example/demo/controller/StudentController.java
+++ LearnEval/src/main/java/com/example/demo/controller/StudentController.java
@@ -7,9 +7,48 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class StudentController {
 	
-	//コピペ用サンプル(ページ表示用メソッド)
 	@RequestMapping(path = "/studentmain", method = RequestMethod.GET)
 	public String mainGet() {
 		return "studentmain";
 	}
+	
+	@RequestMapping(path = "/studentmain", method = RequestMethod.POST)
+	public String mainPost(String page) {
+		switch(page) {
+		case"テスト一覧":
+			return "redirect:/studenttestmenu";
+		case"成績一覧":
+			return "redirect:/studenteval";
+		case"メッセージ一覧":
+			return "redirect:/studentmessage";
+
+		}
+		return "/studentmain";
+	}
+	
+	@RequestMapping(path = "/studenttestmenu", method = RequestMethod.GET)
+	public String testMenuGet() {
+		return "studenttestmenu";
+	}
+	
+	@RequestMapping(path = "/testexam", method = RequestMethod.GET)
+	public String testexGet() {
+		return "testexam";
+	}
+	
+	@RequestMapping(path = "/studenteval", method = RequestMethod.GET)
+	public String evalGet() {
+		return "studenteval";
+	}
+	
+	@RequestMapping(path = "/studenteval", method = RequestMethod.POST)
+	public String evalPOST() {
+		return "redirect:/studentmain";
+	}
+	
+	@RequestMapping(path = "/studentmessage", method = RequestMethod.GET)
+	public String mesGet() {
+		return "studentmessage";
+	}
+	
 }
