9cfbf7e09497484bc5697c3f10db1b919aa09443
S0511 <S0511@20S0511>
2023-12-08 09:50:24 +0900

    共有用

--- LearnEval/src/main/java/com/example/demo/controller/StudentController.java
+++ LearnEval/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,11 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class StudentController {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 	
 	@RequestMapping(path = "/studentmain", method = RequestMethod.GET)
 	public String mainGet() {
@@ -31,9 +40,20 @@ public class StudentController {
 		return "studenttestmenu";
 	}
 	
-	@RequestMapping(path = "/testexam", method = RequestMethod.GET)
-	public String testexGet() {
-		return "testexam";
+	@RequestMapping(path = "/testpage", method = RequestMethod.GET)
+	public String testexGet(Model model) {
+
+
+		//SELECT文の結果をしまうためのリスト
+		List<Map<String, Object>> resultList;
+
+		//SELECT文の実行
+		resultList = jdbcTemplate.queryForList("select * from tests");
+
+		//実行結果をmodelにしまってHTMLで出せるようにする。
+		model.addAttribute("selectResult", resultList);
+		
+		return "testpage";
 	}
 	
 	@RequestMapping(path = "/studenteval", method = RequestMethod.GET)
