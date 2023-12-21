5663aa573f18ca5c630ffedb8d749cd373409340
takuto0115 <takuto.tsaxophone@gmail.com>
2023-12-21 13:58:03 +0900

    12/21 変更前

--- LearnEval/src/main/resources/static/js/anc.js
+++ LearnEval/src/main/resources/static/js/anc.js
@@ -8,7 +8,7 @@ $(function() {
 });
 
 function home(){
-    var result = window.confirm('編集中ですがホームに戻りますか？\n(編集は破棄されます)');
+    var result = window.confirm('編集中ですがホームに戻りますか？\n(編集内容は破棄されます)');
 
     if( result ) {
 		window.location.href = "teachermain";
