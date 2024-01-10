4e00ef87d24c61d4e42b877ea19a45213aad81fe
takuto0115 <takuto.tsaxophone@gmail.com>
2024-01-10 09:43:13 +0900

    1/10 授業開始時

--- LearnEval/src/main/resources/static/js/anc.js
+++ LearnEval/src/main/resources/static/js/anc.js
@@ -15,27 +15,58 @@ function home(){
     }
 }
 
-$(function() {
- 
-  $('button#add').click(function(){
- 
-  var tr_form = '' +
-  '<tr>'+
-      '<td><input type="text" name="text_1[]"></td>'+
-      '<td><input type="text" name="text_2[]"></td>'+
-    '</tr>'+
-    '<tr>'+
-      '<th>C</th>'+
-      '<th>D</th>'+
-    '</tr>'+
-    '<tr>'+
-      '<td><input type="text" name="text_3[]"></td>'+
-      '<td><input type="text" name="text_4[]"></td>'+
-    '</tr>';
- 
-  $(tr_form).appendTo($('table > tbody'));
- 
-});
- 
- 
-});
+//$(function() {
+// 
+//  $("#add").click(function(){
+// 
+//  var tr_form = '' +
+//      '<tr>'+
+//      '<th>A</th>'+
+//      '<th>B</th>'+
+//    '</tr>'+
+//  '<tr>'+
+//      '<td><input type="text" name="1"></td>'+
+//      '<td><input type="text" name="2"></td>'+
+//    '</tr>'+
+//    '<tr>'+
+//      '<th>C</th>'+
+//      '<th>D</th>'+
+//    '</tr>'+
+//    '<tr>'+
+//      '<td><input type="text" name="3"></td>'+
+//      '<td><input type="text" name="4"></td>'+
+//    '</tr>';
+// 
+//  $(tr_form).appendTo($('table > tbody'));
+// 
+//});
+// 
+// 
+//});
+
+
+function createInputFields() {
+      // プルダウンで選択された値を取得
+      var selectedQuantity = document.getElementById("quantity").value;
+
+      // 入力フォームを表示するコンテナの参照を取得
+      var inputContainer = document.getElementById("input-container");
+
+      // コンテナ内の要素をクリア
+      inputContainer.innerHTML = "";
+
+      // 選択された数字分だけ入力フォームを生成してコンテナに追加
+      
+      var q = ['A','B','C','D'];
+      
+      for (var i = 0; i < selectedQuantity; i += 4) {
+		  for(var k : q){
+        var inputField = document.createElement("input");
+        inputField.type = "text";
+        inputField.name = i;
+        }
+        }
+        
+        inputContainer.appendChild(inputField);
+      }
+    }
