11343a48aab40a9d33e86312820491aa36bc9eb5
S0511 <S0511@20S0511>
2024-01-17 09:39:38 +0900

    復活のセッション

--- LearnEval/src/main/java/com/example/demo/controller/LoginController.java
+++ LearnEval/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -25,52 +28,52 @@ public class LoginController {
 	public String checkUser(String type,String id,String pass,Model model,HttpSession session) {
 
 		if("student".equals(type)) {
-			//			
-			//			//SELECT文の結果をしまうためのリスト
-			//			List<Map<String, Object>> result;
-			//
-			//			//SELECT文の実行
-			//			result = jdbcTemplate.queryForList("select * from students where studentID = ? and pass = ?",id,pass);
-			//			
-			//			if (!result.isEmpty()) {
-			//			
-			//			Map<String, Object> user = result.get(0);
-			//			
-			//			String name = (String)user.get("name");
-			//			int studentID = ((Number) user.get("studentID")).intValue();
-			//			
-			//			System.out.println(name);
-			//			
-			//			session.setAttribute("name",name);
-			//			session.setAttribute("studentID",studentID);
-			//			
+						
+						//SELECT文の結果をしまうためのリスト
+						List<Map<String, Object>> result;
+			
+						//SELECT文の実行
+						result = jdbcTemplate.queryForList("select * from students where studentID = ? and pass = ?",id,pass);
+						
+						if (!result.isEmpty()) {
+						
+						Map<String, Object> user = result.get(0);
+						
+						String name = (String)user.get("name");
+						int studentID = ((Number) user.get("studentID")).intValue();
+						
+						System.out.println(name);
+						
+						session.setAttribute("name",name);
+						session.setAttribute("studentID",studentID);
+						
 			return "redirect:/studentmain";
 		}
-		/*}*/
+		}
 		else if(type.equals("teacher")) {
 
-			//			
-			//			//SELECT文の結果をしまうためのリスト
-			//			List<Map<String, Object>> result;
-			//			
-			//			//SELECT文の実行
-			//			result = jdbcTemplate.queryForList("select * from teachers where teacherID = ? and pass = ?",id,pass);
-			//			
-			//			if (!result.isEmpty()) {
-			//			
-			//			Map<String, Object> user = result.get(0);
-			//			
-			//			String name = (String)user.get("name");
-			//			int teacherID = ((Number) user.get("teacherID")).intValue();
-			//
-			//			System.out.println(name);
-			//			
-			//			session.setAttribute("name",name);
-			//			session.setAttribute("teacherID",teacherID);
+						
+						//SELECT文の結果をしまうためのリスト
+						List<Map<String, Object>> result;
+						
+						//SELECT文の実行
+						result = jdbcTemplate.queryForList("select * from teachers where teacherID = ? and pass = ?",id,pass);
+						
+						if (!result.isEmpty()) {
+						
+						Map<String, Object> user = result.get(0);
+						
+						String name = (String)user.get("name");
+						int teacherID = ((Number) user.get("teacherID")).intValue();
+			
+						System.out.println(name);
+						
+						session.setAttribute("name",name);
+						session.setAttribute("teacherID",teacherID);
 
 			return "redirect:/teachermain";
 		}
-		/*}*/
+		}
 		else{
 			model.addAttribute("login","<script>alert('一致するIDがありません');</script>");
 		}
